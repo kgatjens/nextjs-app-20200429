ba2418ad73646128f804dca04f30ebbc032c43d7
kbrenes <contact@kennethbrenes.com>
2021-05-06 09:38:43 -0600

    Layout changes in index

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,18 @@ import { getAllPostsForHome } from '../lib/api'
 
 import styles from '../styles/Home.module.css'
 
-export default function Index({ allPosts: { edges }, preview }) {
+export default function Index({ allPosts: { edges } }) {
   const heroPost = edges[0]?.node
   
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
           <title>Next.js Blog Example with WP - GraphQL</title>
+          <meta property="og:title" content={heroPost.title} key="title" />
+         {/* <meta property="og:description" content={description} key="ogdesc" />
+         <meta name="description" content={description}></meta> */}
+
         </Head>
         <Container>
           <Intro />
