29e31436cdfccfb69b2a1fa33b6929c26496011d
kbrenes <contact@kennethbrenes.com>
2021-05-10 16:03:03 -0600

    Test for prod env variables 2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    serverRuntimeConfig: {
-    //   // Will only be available on the server side
-    //   mySecret: 'secret',
-    //   secondSecret: process.env.SECOND_SECRET, // Pass through env variables
-    },
-    publicRuntimeConfig: {
-      // Will be available on both server and client
-      staticFolder: '/static',
-      apiUrl='https://gorillalogic.com/graphql'
+    // serverRuntimeConfig: {
+    // //   // Will only be available on the server side
+    // //   mySecret: 'secret',
+    // //   secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+    // },
+    // publicRuntimeConfig: {
+    //   // Will be available on both server and client
+    //   staticFolder: '/static',
+    //   apiUrl='https://gorillalogic.com/graphql'
 
-    },
-    env: {
-        customKey: 'https://gorillalogic.com/graphql',
-      },
+    // },
+    // env: {
+    //     customKey: 'https://gorillalogic.com/graphql',
+    //   },
   }
